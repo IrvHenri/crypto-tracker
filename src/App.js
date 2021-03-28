791ed5a70bc2455858fcada4b5c6ee4073c8c512
Irving <henriquezirv@gmail.com>
2021-03-28 00:36:19 -0400

    added coin component and rendered to main feed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import { Coin } from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     axios
       .get(
@@ -12,12 +14,44 @@ function App() {
       .then((res) => {
         setCoins(res.data);
         console.log(res.data);
-      });
-  });
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
-    <div className="App">
-      <h1>apiii</h1>
+    <div className="coin-app">
+      <main className="coin-search">
+        <h1>Coin Search</h1>
+        <form>
+          <input
+            className="coin-input"
+            placeholder="Search"
+            type="text"
+            onChange={handleChange}
+          />
+        </form>
+      </main>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
     </div>
   );
 }
