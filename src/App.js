ecc4bb97ce6ae17b9b6a97259386943bdc062210
Irving <henriquezirv@gmail.com>
2021-03-30 01:08:47 -0400

    added tablehead sorting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Hamburger from "hamburger-react";
+import { useSortableData } from "./hooks/useSortableData";
 import "./App.css";
-import { Coin } from "./Coin";
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
   useEffect(() => {
@@ -13,7 +15,6 @@ function App() {
       )
       .then((res) => {
         setCoins(res.data);
-        console.log(res.data);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -22,37 +23,75 @@ function App() {
     setSearch(e.target.value);
   };
 
+  const openBurger = () => {
+    setOpen(!isOpen);
+  };
+
   const filteredCoins = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
+  const { items, requestSort } = useSortableData(filteredCoins);
 
   return (
     <div className="coin-app">
       <main className="coin-search">
-        <h1>Coin Search</h1>
+        <h1>Coin Tracker</h1>
         <form>
           <input
             className="coin-input"
-            placeholder="Search"
+            placeholder="What are you looking for?"
             type="text"
             onChange={handleChange}
           />
+          <Hamburger onClick={() => openBurger()} size={20} />
         </form>
       </main>
-      {filteredCoins.map((coin) => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            price={coin.current_price}
-            image={coin.image}
-            symbol={coin.symbol}
-            marketCap={coin.market_cap}
-            priceChange={coin.price_change_percentage_24h}
-            totalVolume={coin.total_volume}
-          />
-        );
-      })}
+      <table>
+        <caption>Cryptocurrencies</caption>
+        <thead>
+          <tr>
+            <th>
+              <button onClick={() => requestSort("name")}>Name</button>
+            </th>
+            <th>
+              <button onClick={() => requestSort("current_price")}>
+                Price
+              </button>
+            </th>
+            <th>
+              <button
+                onClick={() => requestSort("price_change_percentage_24h")}
+              >
+                24%
+              </button>
+            </th>
+            <th>
+              <button onClick={() => requestSort("market_cap")}>
+                Market Cap
+              </button>
+            </th>
+            <th>
+              <button onClick={() => requestSort("total_volume")}>
+                Total Volume
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <img src={coin.image} alt=" coin logo" />
+                {coin.name} {coin.symbol}
+              </td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.market_cap}</td>
+              <td>{coin.total_volume}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
